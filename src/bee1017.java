0ea016dc824e46adfaf32759aefbd9a28976ddb0
Fabíola Alves <neves.fabiolaf@gmail.com>
2025-06-10 09:31:14 -0300

    1017 - Gasto de Combustível

---
+++ src/bee1017.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class bee1017 {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int tempoGasto = sc.nextInt();
+        int velocidadeMedia = sc.nextInt();
+
+        int distancia = tempoGasto * velocidadeMedia;
+        double combustivelGasto = distancia / 12.0;
+
+        System.out.printf("%.3f%n", combustivelGasto);
+
+        sc.close();
+    }
+}
